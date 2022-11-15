e76107485f2d3680b19140794d2420ae08d8793d
Rahasmi <116746703+Rahasmi@users.noreply.github.com>
2022-11-15 16:17:24 +0530

    Add files via upload

---
+++ Razi Ahmad Hasmi/src/main/java/com/example/broadcastapp/MyReceiver.java
@@ -0,0 +1,21 @@
+package com.example.broadcastapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    public MyReceiver() {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        //throw new UnsupportedOperationException("Not yet implemented");
+        Toast.makeText(context, "Phone is connected to power", Toast.LENGTH_LONG).show();
+    }
+}
