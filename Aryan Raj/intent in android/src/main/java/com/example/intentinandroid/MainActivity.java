67be4a439ebb79e95191248e6a5fe350c947f965
ARYAN RAJ <109868807+aryanraj208@users.noreply.github.com>
2022-11-24 22:51:35 +0530

    Add files via upload

---
+++ Aryan Raj/intent in android/src/main/java/com/example/intentinandroid/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.intentinandroid;
+
+import static android.net.Uri.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt;
+
+    EditText et;
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et = (EditText)findViewById(R.id.editTextPhone);
+
+        bt = (Button)findViewById(R.id.button2);
+
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+                String number = et.getText().toString();
+
+
+                Intent callintent = new Intent(Intent.ACTION_CALL);
+                callintent.setData(Uri.parse("tel:"+number));
+
+
+                if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED)
+                {
+                    Toast.makeText(getApplicationContext(), "Please Allow!", Toast.LENGTH_SHORT).show();
+
+
+
+                    return;
+
+
+
+                    
+                }
+
+
+                startActivity(callintent);
+
+            }
+        });
+
+    }
+}
