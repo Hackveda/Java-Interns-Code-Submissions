2c2d62ad265b667759d300568d32b4dca8572c5a
dilipgr123 <118910509+dilipgr123@users.noreply.github.com>
2023-01-01 12:01:37 +0530

    Add files via upload

---
+++ Dilip G R/broadcastapp/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.broadcastapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        Toast.makeText(context, "phone is connected to power", Toast.LENGTH_SHORT).show();
+    }
+}
