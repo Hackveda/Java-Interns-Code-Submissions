5eff3c5e342f83ea1070085080138a2f648790d4
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-01-21 14:36:20 +0530

    Create abstract_class.java

---
+++ Parth_Kharva/abstract/abstract_class.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abstract_method;
+
+/**
+ *
+ * @author Parthkharva
+ */
+abstract class abstract_class {
+    public void method1(){
+        System.out.println("hi");
+    }
+    abstract public void method2();
+}
