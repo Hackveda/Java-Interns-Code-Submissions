3626698df1c8c888b9546c604fe711ba645b81f4
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-01-23 19:28:14 +0530

    Finding of mean using 1d array code is implementation by Parth Kharva
    Practical-6 : find mean using 1d array

---
+++ Parth_Kharva/Fiind_Mean_values_using_1D_Array.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package find_mean_with_1d_array;
+
+/**
+ *
+ * @author Parthkharva
+ */
+public class find_mean_with_1d_array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] num = new int[2];
+        num[0]=34;
+        num[1]=24;
+        
+        int mean = (num[0]+num[1])/2;
+        
+        System.out.println("Two array values of mean is : "+mean+".");
+    }
+    
+}
