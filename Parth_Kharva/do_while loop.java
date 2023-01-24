99bf92eee658007066c398a94ec803882220fba5
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-01-24 23:30:50 +0530

    do_while loop implemented by Parth Kharva
    Practical- 12 : do while loop

---
+++ Parth_Kharva/do_while loop.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package do_while;
+
+/**
+ *
+ * @author Parthkharva
+ */
+public class Do_while {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+    
+}
