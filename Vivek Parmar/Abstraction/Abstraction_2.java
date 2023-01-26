874da982c76084e35028fd2d45f6267c11cf9c2e
Vivek Parmar <76220622+vivek1917@users.noreply.github.com>
2023-01-26 19:08:38 +0530

    Abstraction
    Abstraction method example code.

---
+++ Vivek Parmar/Abstraction/Abstraction_2.java
@@ -0,0 +1,11 @@
+package com.vivek.javacodes;
+
+abstract class Abstraction_2 {
+
+	public void myMethod() {
+		System.out.println("Abstraction-2.");
+	}
+	
+	abstract public void anotherMethod();
+
+}
