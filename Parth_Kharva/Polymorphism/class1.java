f28da0cef7248041770328b51bfbf20ca88a8c83
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-01-27 17:02:08 +0530

    class 1 implemented by Parth Kharva
    Practical-18 : class1

---
+++ Parth_Kharva/Polymorphism/class1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package polymorphism;
+
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+
+/**
+ *
+ * @author Parthkharva
+ */
+
+ class pnb implements bank {
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public float rate_of_interest(){
+        return 956.8f;
+    } 
+}
