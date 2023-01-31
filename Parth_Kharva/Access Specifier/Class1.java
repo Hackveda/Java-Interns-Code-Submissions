83b58306f92ff0d85d9b6fa0d37517a39455475c
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-01-31 14:56:39 +0530

    class1 implemented by Parth Kharva
    Practical - 18 : access specifier

---
+++ Parth_Kharva/Access Specifier/Class1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package access_specifer;
+
+/**
+ *
+ * @author Parthkharva
+ */
+public class asscess_specifer_creation {
+    public int a = 45;
+    private int b =96;
+    protected int c = 78;
+    int d = 80;
+}
