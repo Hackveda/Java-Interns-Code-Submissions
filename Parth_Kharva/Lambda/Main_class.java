27ff923a06a9431449e4197247283a9a48203720
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-02-02 13:51:23 +0530

    Main_class1 implemented by Parth Kharva
    Practical - 21 : Main class

---
+++ Parth_Kharva/Lambda/Main_class.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lambda;
+
+/**
+ *
+ * @author Parthkharva
+ */
+public class Lambda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        practice_1 obj1 =(int a)->{System.out.println(2*a);}; 
+        obj1.abstractfunction(25);
+        obj1.normalfunction();
+    }
+    
+}
