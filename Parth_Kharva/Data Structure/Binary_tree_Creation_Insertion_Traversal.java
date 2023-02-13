426ab5ab72977bed678ed5decf3f062eb03040c0
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-02-13 13:09:02 +0530

    Binary_tree_Creation_Insertion_Traversal implemented By Parth Kharva
    Practical - 13 of DSA : Binary_tree_Creation_Insertion_Traversal

---
+++ Parth_Kharva/Data Structure/Binary_tree_Creation_Insertion_Traversal.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package binary_tree_creation_insert_traversal;
+
+import binary_tree_creation_insert_traversal.Binary_tree_creation_insert_traversal.Tree.Node;
+
+/**
+ *
+ * @author Parthkharva
+ */
+public class Binary_tree_creation_insert_traversal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        Node root = new Node(50);
+        tree.insert(root, 12);
+        tree.insert(root, 1);
+        tree.insert(root, 26);
+        tree.insert(root, 10);
+        tree.insert(root, 2);
+        tree.insert(root, 62);
+        tree.insert(root, 152);
+        tree.insert(root, 129);
+        tree.insert(root, 17);
+        tree.insert(root, 100);
+        
+        System.out.println("Travers the tree :");
+        tree.traversal_inorder(root);
+    }
+
+    public static class Tree {
+
+        static class Node {
+
+            int value;
+            Node left, right;
+
+            Node(int value) {
+                this.value = value;
+
+                left = null;
+                right = null;
+            }
+
+        }
+        public void insert(Node node,int value){
+            if(value<node.value){
+                if(node.left != null){
+                    insert(node.left,value);
+                }
+                else{
+                    System.out.println("Insert "+value +" to the left of "+node.value+".");
+                    node.left = new Node(value);
+                }
+            }
+            else if(value>node.value){
+                if(node.right != null){
+                    insert(node.right,value);
+                }
+                else{
+                    System.out.println("Insert "+value +" to the right of "+node.value+".");
+                    node.right = new Node(value);
+                }
+            } 
+        }
+        
+        public void traversal_inorder(Node node){
+            if(node != null){
+                traversal_inorder(node.left);
+                System.out.println(" "+node.value);
+                traversal_inorder(node.right);
+            }
+        }
+    }
+
+}
