408e775545112aa38c48cb869ee303fda82226b1
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-03-11 12:01:25 +0530

    Create SpringbootApplication.java

---
+++ Parth_Kharva/Spring boot/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.java.springboot.test.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojectApplication extends simple_controller{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectApplication.class, args);
+		
+	}
+
+}
