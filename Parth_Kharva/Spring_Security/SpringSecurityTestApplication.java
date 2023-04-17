eeb7a4a8646ecb89fd0b82086fb14518bf3db032
Parthkharva69 <99704172+Parthkharva69@users.noreply.github.com>
2023-04-17 12:36:40 +0530

    SpringSecurityTestApplication.java implemented by Parth Kharva
    Project : Spring Security

---
+++ Parth_Kharva/Spring_Security/SpringSecurityTestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.test.spring_security_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTestApplication.class, args);
+	}
+
+}
